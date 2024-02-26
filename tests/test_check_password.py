984de3e5a1ad8063facb7d44b13ad222d6f3f020
sewerynszydelko <seweryn494a@gmail.com>
2024-02-26 17:49:22 +0100

    Create test for hase words method

--- tests/test_check_password.py
+++ tests/test_check_password.py
@@ -1,5 +1,5 @@
 """ Test check password file """
-from scripts.check_password import read_file_paswords, check_passwords_strength, save_safe_passwords
+from scripts.check_password import read_file_paswords, check_passwords_strength, save_safe_passwords, hashe_words
 
 test_path = "Path to your file test"
 
@@ -12,6 +12,17 @@ def test_read_file_is_list():
 
 def test_check_password_list():
 
-    test_bool_list = check_passwords_strength(["Example1!", "example1"])
+    test_words_list = check_passwords_strength(["Example1!", "example1"])
 
-    assert test_bool_list == ["Example1!"]
+    assert test_words_list == ["Example1!"]
+
+
+def test_hase_words():
+    test_words_list = ["Example1!", "example1"]
+    test_words_same_list = ["Example2","Example2"]
+
+    test_1_result = hashe_words(test_words_list)
+    test_2_result = hashe_words(test_words_same_list)
+
+    assert type(test_1_result) == list
+    assert test_2_result[0] == test_2_result[1]
