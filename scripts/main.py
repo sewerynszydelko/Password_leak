82dee3f2e8cc065be68f386f9cadd5447841edbf
sewerynszydelko <seweryn494a@gmail.com>
2024-03-12 09:02:13 +0100

    Added about func , get user input to exti script

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,6 @@
 """Password leaked main script"""
 from password_validator.validators import PasswordValidator, ValidationError
+import sys
 
 
 def password_check(path_to_save="safe_pass.txt", path_to_read="passwords.txt") -> None:
@@ -23,5 +24,25 @@ def password_check(path_to_save="safe_pass.txt", path_to_read="passwords.txt") -
                 print(error, " : ", password_striped)
 
 
+def about() -> None:
+    print("Welcom in password safty script!")
+    print("In this program i will check if you'r password pass all validations")
+    print("If you password pass i will save in same directory in a file 'safe_pass/txt'")
+    print("Password that you given to me i will read from 'passwords.txt' placed in same directory as script file")
+
+
+def get_user_choice():
+    while True:
+        try:
+            user_input = int(input("If you want to exit just type '0'\nor any other number to contiune: "))
+            if user_input == 0:
+                sys.exit
+            break
+        except ValidationError:
+            print("You give me non of numbers , pleas do so")
+
+
 if __name__ == "__main__":
+    about()
+    get_user_choice()
     password_check()
